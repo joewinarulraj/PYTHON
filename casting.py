16447ca79e426c9ae6c7c05ff29021694693dee8
joewinarulraj <joewinarulraj@gmail.com>
2025-11-05 12:05:47 +0530

    python

---
+++ casting.py
@@ -0,0 +1,34 @@
+a=int("10")
+b=int("20")
+c=a+b
+print(c)
+
+a=input()
+b=input()
+c=a+b
+print(c)
+
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
+
+
+a=input("name:")
+b=int(input("age:"))
+c=input()
+print("my name is :" , a)
+print("my age is:" , b)
+print("my address:",c)
+
+
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=a*b*c
+e=a+b+c
+f=d/e
+print(f)
+
