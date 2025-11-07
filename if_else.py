0e66764d5a77f2798662dffe4e355219b327f2df
joewinarulraj <joewinarulraj@gmail.com>
2025-11-07 19:34:54 +0530

    python

--- if_else.py
+++ if_else.py
@@ -2,4 +2,31 @@ x=int(input("enter your age:"))
 if x>18:
     print("your are eligible for voting")
 else:
-    print("your are not eligible for voting")    
+    print("your are not eligible for voting")  
+
+
+
+
+mark=int(input("enter your mark:"))
+if(mark>=35):
+    print("pass")
+else:
+    print("fail")
+
+
+
+income=int(input("enter your income"))
+if(income<7000):
+    print("your are eligible for scholarship")
+else:
+    print("you are not eligible for scholarship")
+
+
+
+
+
+num=int(input("enter a number:"))
+if(num%2==0):
+    print(num," is divisible by 2")
+else:
+    print(num,"is not divisible by 234")
